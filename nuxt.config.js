da7626f4e6183e15f79ed0296aa19efa58910424
daxyonis <emaciejko@gmail.com>
2020-09-07 16:17:02 -0400

    Added favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,8 +26,11 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    link: [      
+      {rel:"apple-touch-icon", sizes:"180x180", href:"/apple-touch-icon.png"},
+      {rel:"icon", type:"image/png", sizes:"32x32", href:"/favicon-32x32.png"},
+      {rel:"icon", type:"image/png", sizes:"16x16", href:"/favicon-16x16.png"},
+      {rel:"manifest", href:"/site.webmanifest"}
     ]
   },
   /*
